f679ccce4f3b9e1dfe77ccde02902049f35437aa
daniel.bradea <daniel.bradea@ferendin.com>
2024-07-08 13:48:50 +0300

    first commit

---
+++ src/test/java/com/bid90/msemail/MsemailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid90.msemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsemailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
